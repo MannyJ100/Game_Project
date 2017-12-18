1d6f82c6461ac042c129f375cb32b4f1c9f58ded
Tech-A44 <tech-a44@Tech-A44s-Air.lan>
2017-12-18 19:44:19 +0000

    work after class monday

--- game.js
+++ game.js
@@ -1,12 +1,16 @@
 $(function() {
 console.log("before global");
 
+var placeholders
 var boxes = document.getElementsByTagName("td");
 var turnText = document.querySelector(".playerTurn");
 var counter = 1;
 var RedMoves = [];
 var YellowMoves = [];
-var table = $('table tr');
+// var row = document.getElementsByTagName("tr");
+// var validMoves = [];
+// var x01 = [02, 08, 09];
+// var x02 = [03, 10, 11];
 
 function start() {
 	console.log("begin");
@@ -17,12 +21,16 @@ function start() {
 function addRedandYellowListener() {
 	for (var i = boxes.length - 1; i >= 0; i--) {
 		boxes[i].addEventListener("click", addRedorYellow);
+
 	}
 }
 addRedandYellowListener();
 
 function addRedorYellow(event) {
 	if (event.target.innerHTML.length === 0) {
+		// checkValidMove(event);/*target.id,tdelement);*/
+
+		// console.log(event.target.id);
 		if (counter % 2 === 0) {
 			YellowMoves.push(parseInt(event.target.getAttribute("data-num")));
 			event.target.innerHTML = "Yellow";
@@ -42,13 +50,33 @@ function addRedorYellow(event) {
 		}
 	} 
 
-	function checkBottom(colIndex) {
-		var colourReport = addRedorYellow(5,colIndex);
+	// function checkValidMove(value) {
+	// 	var Check = ["08","09","02"]; 
+	// 	if (value.target.id === "01") {
+
+	// 		value.target.setAttribute("class", "Red");
+	// 		for (var x = 0; x < x01.length; x++);
+	// 			validMoves.push(x01[x])
+	// 		return;
+	// 	}
+	// 	if (value.target.id === "02") {
+
+	// 		value.target.setAttribute("class", "Yellow");
+	// 		for (var x = 0; x < x02.length; x++);
+	// 			validMoves.push(x02[x])
+	// 		return;
+	// 	}
+          
+	// 	if (x01[0] == "02" || x01[1] == "08" || x01[2] == "09") {
+ //           value.target.setAttribute("class", "Yellow");
+		      
+	// 	}
+	// }
+
+	function checkBottom() {
+		var colourReport = addRedorYellow(5);
 		for (var row = 5; row > -1; row--) {
-			colourReport = addRedorYellow(row,colIndex);
-			if (colourReport === null) {
-				return row
-			}
+			colourReport = addRedorYellow(row);
 		}
 	}
 })
