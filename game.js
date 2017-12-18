f545e651a93fa1ef3ec85b83a0f20fcb054ccdc4
Tech-A44 <tech-a44@Tech-A44s-MacBook-Air.local>
2017-12-18 13:29:22 +0000

    lunch day 1

--- game.js
+++ game.js
@@ -3,4 +3,39 @@ console.log("before global");
 
 var boxes = document.getElementsByTagName("td");
 var turnText = document.querySelector(".playerTurn");
+var counter = 1;
+
+function start() {
+	console.log("begin");
+	addRedandYellowListener();
+	addResetListener();
+}
+
+function addRedandYellowListener() {
+	for (var i = boxes.length - 1; i >= 0; i--) {
+		boxes[i].addEventListener("click", addRedorYellow);
+	}
+}
+
+function addRedorYellow(event) {
+	if (event.target.innerHTML.length === 0) {
+		if (counter % 2 === 0) {
+			YellowMoves.push(parseInt(event.target.getAttribute("data-num")));
+			event.target.innerHTML = "Yellow";
+			event.target.setAttribute("class","Yellow");
+			turnText.innerHTML = "It is Player Yellow's turn";
+			counter++;
+			// checkforwin
+		}
+		else {
+			RedMoves.push(parseInt(event.target.getAttribute("data-num")));
+			event.target.innerHTML = "Red";
+			event.target.setAttribute("class", "Red");
+			turnText.innerHTML = "It is Player Red's turn"
+			counter++
+			// checkforwin
+		}
+	}
+} 
 })
+
