bcdba3e41eec1bae9b572e3968e2eee8e0283b8e
Tech-A44 <tech-a44@Tech-A44s-Air.lan>
2017-12-19 22:03:46 +0000

    game work end of day 2

--- game.js
+++ game.js
@@ -1,13 +1,6 @@
 $(function() {
 console.log("before global");
 
-// var placeholders
-
-// var boxes = document.getElementsByTagName("td");
-// var turnText = document.querySelector(".playerTurn");
-// var counter = 1;
-// var RedMoves = [];
-// var YellowMoves = [];
 var playerRed = "Player Red"
 var playerYellow = "Player Yellow"
 var player1Color = 'rgb(255, 0, 0)'
@@ -15,104 +8,32 @@ var player2Color = 'rgb(255, 252, 0)'
 var gameOn = true;
 var table = $('table tr');
 
-// var row = document.getElementsByTagName("tr");
-// var validMoves = [];
-// var x01 = [02, 08, 09];
-// var x02 = [03, 10, 11];
 
-// function start() {
-// 	console.log("begin");
-// 	addRedandYellowListener();
-// 	addResetListener();
-// }
-
-// function addRedandYellowListener() {
-// 	for (var i = boxes.length - 1; i >= 0; i--) {
-// 		boxes[i].addEventListener("click", addRedorYellow);
-// 	}
-// }
-
-// addRedandYellowListener();
-
-// function addRedorYellow(event) {
-// 	if (event.target.innerHTML.length === 0) {
-// 		// checkValidMove(event);/*target.id,tdelement);*/
-
-// 		// console.log(event.target.id);
-// 		if (counter % 2 === 0) {
-// 			YellowMoves.push(parseInt(event.target.getAttribute("data-num")));
-// 			event.target.innerHTML = "Yellow";
-// 			event.target.setAttribute("class","Yellow");
-// 			turnText.innerHTML = "It is Player Red's turn";
-// 			counter++;
-// 			// checkforwin
-// 		}
-// 		else {
-// 			RedMoves.push(parseInt(event.target.getAttribute("data-num")));
-// 			event.target.innerHTML = "Red";
-// 			event.target.setAttribute("class", "Red");
-// 			turnText.innerHTML = "It is Player Yellow's turn"
-// 			counter++
-// 			// checkforwin
-// 			}
-// 		}
-// 	} 
-
-	// function checkValidMove(value) {
-	// 	var Check = ["08","09","02"]; 
-	// 	if (value.target.id === "01") {
-
-	// 		value.target.setAttribute("class", "Red");
-	// 		for (var x = 0; x < x01.length; x++);
-	// 			validMoves.push(x01[x])
-	// 		return;
-	// 	}
-	// 	if (value.target.id === "02") {
-
-	// 		value.target.setAttribute("class", "Yellow");
-	// 		for (var x = 0; x < x02.length; x++);
-	// 			validMoves.push(x02[x])
-	// 		return;
-	// 	}
-          
-	// 	if (x01[0] == "02" || x01[1] == "08" || x01[2] == "09") {
- //           value.target.setAttribute("class", "Yellow");
-		      
-	// 	}
-	// }
+	// identify td location in table to change colour. Finds cell
 	function DiffColor(rowI,colI,color) {
-		return table.eq(rowI).find('td').eq(colI).css('background-color',color)
+		return table.eq(rowI).find('td').eq(colI).css('background-color',color);
 	}
 
+	// Someting to tell us current colour of button
 	function returnColor(rowI,colI){
 		return table.eq(rowI).find('td').eq(colI).css('background-color');
 	}
 
+	// Find bottom avail free slot
 	function lowestFree(colI) {
-		var colorReport = returnColor(5,colI);
+	
+		var colorFeedback = returnColor(5,colI);
 		for (var row = 5; row > -1; row--) {
-			colorReport = returnColor(row,colI);
-			if (colorReport === 'rgb(128, 128, 128)') {
+			colorFeedback = returnColor(row,colI);
+			if (colorFeedback === 'rgb(128, 128, 128)') {
 				return row
 			}
 		}
 	}
 
-	// function addResetListener(){
-	// 	var resetButton = document.getELementById("Reset");
-	// 	ResetButton.addEventListener("click", ResetBoard);
-	// }
-
-	// function ResetBoard(){
-	// 	for (var i = boxes.length - 1; i >= 0; i--) {
-	// 		boxes[i].innerHTML="";
-	// 		boxes[i].setAttribute("class","clear");
-	// 	}
-	// }
-	
-
+	// check if for inputs have the same colour
 	function colorMatch(a,b,c,d) {
-		return (a === b && a === c && a === d && a !== 'rgb(128, 128, 128)' && a !== undefined)
+		return (a === b && a === c && a === d && a !== 'rgb(128, 128, 128)' && a !== undefined);
 	}
 
 	function chkWinVert(){
@@ -121,56 +42,43 @@ var table = $('table tr');
 			for (var col = 0; col < 7; col++){
 				if (colorMatch(returnColor(row,col), returnColor(row+1,col), returnColor(row+2,col), returnColor(row+3,col))){
 					console.log("hihi");
-					// reportWin(row,col);
 					return true;
-					} else {
-						continue;
+					}	else {
+						console.log("no match");
 					}
 				}
 			}	
 	}
-		function chkWinHor() {// Horizontal 	
+		
+		// Horizontal
+	function chkWinHor() {	
 		for (var row = 0; row < 6; row++){
 			for (var col = 0; col < 4; col++){
 				if (colorMatch(returnColor(row,col), returnColor(row,col+1), returnColor(row,col+2), returnColor(row,col+3))) {
-				// sendWin(row,col);
-				return true;
-			} else {
-				continue;
+					return true;
+					} 	else {
+						console.log("no match");
+					}
 				}
-			}
-		}		
+			}		
 	}	
-		// Diag R
-		function chkWinDiag() {
+		
+		// Diagonal
+	function chkWinDiag() {
 		for (var row = 0; row < 7; row++){
 			for (var col = 0; col < 5; col++){				
 				if (colorMatch(returnColor(row,col), returnColor(row+1,col+1), returnColor(row+2,col+2), returnColor(row+3,col+3))) {
-				// sendWin(row,col);
-				return true;
-			} else if (colorMatch(returnColor(row,col), returnColor(row-1,col+1), returnColor(row-2,col+2), returnColor(row-3,col+3))) {
-				// sendWin(row,col);
-				return true;
-			} else {
-				continue;
+					return true;
+					}	else if (colorMatch(returnColor(row,col), returnColor(row-1,col+1), returnColor(row-2,col+2), returnColor(row-3,col+3))) {
+					return true;
+					}	else {
+						console.log("no match");
+					}
 				}
-			}
-		}	
+			}	
 	 }
-		// Diag L
-	// 	function chkWinDiagL() {
-	// 	for (var row = 3; row < 6; row++);{
-	// 		for (var col = 0; col <4; col++);{
-	// 			if (colorMatch(returnColor(row,col), returnColor(row-1,col+1), returnColor(row-2,col+2), returnColor(row-3,col+3))) {
-	// 			// sendWin(row,col);
-	// 			return true;
-	// 		} else {
-	// 			continue
-	// 			}
-	// 		}
-	// 	}	
-	// }
-
+		
+		// Current player
 	var currentPlayer = 1;
 	var currentName = playerRed;
 	var currentColor = player1Color;
@@ -182,24 +90,24 @@ var table = $('table tr');
 
 	DiffColor(bottomFree,col,currentColor);
 
-	if (chkWinVert() || chkWinHor() || chkWinDiag()) {
-		alert("You Have Won");
-	}
+		if (chkWinVert() || chkWinHor() || chkWinDiag()) {
+			alert("You Have Won");
+		}
 
+		//Player switch 
 	currentPlayer = currentPlayer * -1
 
-	if (currentPlayer === 1) {
-		currentName = playerRed;
-		$('h2').text(currentName + " it is your turn to connect")
-		currentColor = player1Color;
-	}	else {
-		currentName = playerYellow;
-		$('h2').text(currentName + " it is your turn to connect")
-		currentColor = player2Color;
+		if (currentPlayer === 1) {
+			currentName = playerRed;
+			$('h2').text(currentName + " it is your turn to connect")
+			currentColor = player1Color;
+		}	else {
+			currentName = playerYellow;
+			$('h2').text(currentName + " it is your turn to connect")
+			currentColor = player2Color;
 
-	}
+		}
 	})
 
 
 })
-
